a012326265a8043b4b3ab9eeecff279fa3d5f686
yeonseo <yeonseo.jung0@gmail.com>
2024-07-24 15:46:24 +0900

    [feat] "/register" 접근 허용

--- backend/pinokkio/src/main/java/com/example/pinokkio/config/SecurityConfig.java
+++ backend/pinokkio/src/main/java/com/example/pinokkio/config/SecurityConfig.java
@@ -44,6 +44,7 @@ public class SecurityConfig {
     };
 
     private final String[] POST_PERMIT_API_URL = {
+            "/register/**",
             "/refresh",
             "/users/auth/token/"
     };
