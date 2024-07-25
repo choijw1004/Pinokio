859cc3fe92bb268bac7fca77817824a4f44ced2c
yeondori <yeonseo.jung0@gmail.com>
2024-07-25 13:02:40 +0900

    [fix] @AllArgsConstructor 제거

--- backend/pinokkio/src/main/java/com/example/pinokkio/api/mail/request/MailRequest.java
+++ backend/pinokkio/src/main/java/com/example/pinokkio/api/mail/request/MailRequest.java
@@ -1,12 +1,10 @@
 package com.example.pinokkio.api.mail.request;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
-@AllArgsConstructor
 public class MailRequest {
     private String email;
 }
