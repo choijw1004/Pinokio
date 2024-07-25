2fcb118322506a8f3707e417d00f7cc3f3e49b50
yeonseo <yeonseo.jung0@gmail.com>
2024-07-25 10:13:20 +0900

    [feat] fromValue 메서드 추가

--- backend/pinokkio/src/main/java/com/example/pinokkio/config/jwt/Role.java
+++ backend/pinokkio/src/main/java/com/example/pinokkio/config/jwt/Role.java
@@ -2,7 +2,7 @@ package com.example.pinokkio.config.jwt;
 
 public enum Role {
     P("ROLE_POS"),
-    K("KIOSK"),
+    K("ROLE_KIOSK"),
     T("ROLE_TELLER");
 
     private final String value;
@@ -13,5 +13,14 @@ public enum Role {
 
     public String getValue() {
         return value;
+    }
+
+    public static Role fromValue(String value) {
+        for (Role role : values()) {
+            if (role.getValue().equals(value)) {
+                return role;
+            }
+        }
+        throw new IllegalArgumentException("No enum constant with value " + value);
     }
 }
