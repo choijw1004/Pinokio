5bd9b78056634bd912d6c35562927412ec86511a
yeonseo <yeonseo.jung0@gmail.com>
2024-07-30 17:38:27 +0900

    [refactor] 불필요한 import 삭제

--- backend/pinokkio/src/main/java/com/example/pinokkio/api/pos/code/CodeRepository.java
+++ backend/pinokkio/src/main/java/com/example/pinokkio/api/pos/code/CodeRepository.java
@@ -2,8 +2,6 @@ package com.example.pinokkio.api.pos.code;
 
 import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.UUID;
 
@@ -11,6 +9,7 @@ public interface CodeRepository extends JpaRepository<Code, UUID> {
 
     /**
      * 입력받은 식별자에 부합하는 코드 존재 여부를 반환한다.
+     *
      * @param id 식별자 정보
      * @return 입력받은 식별자에 부합하는 코드 존재 여부
      */
