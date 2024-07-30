96d84ff8c4f1aaa6efa36d2642dcf42a15697614
yeonseo <yeonseo.jung0@gmail.com>
2024-07-30 11:14:42 +0900

    [feat] Response DTO 추가

---
+++ backend/pinokkio/src/main/java/com/example/pinokkio/api/room/dto/request/RoomInfo.java
@@ -0,0 +1,14 @@
+package com.example.pinokkio.api.room.dto.request;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED, force = true)
+public class RoomInfo {
+    private String roomId;
+    private String tellerId;
+}
