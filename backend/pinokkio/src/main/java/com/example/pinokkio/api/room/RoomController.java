63109cca0267bae6a6535bed2d33cb35dafc460f
yeonseo <yeonseo.jung0@gmail.com>
2024-07-30 11:15:11 +0900

    [refactor] Controller - Service 로직 분리

--- backend/pinokkio/src/main/java/com/example/pinokkio/api/room/RoomController.java
+++ backend/pinokkio/src/main/java/com/example/pinokkio/api/room/RoomController.java
@@ -1,27 +1,16 @@
 package com.example.pinokkio.api.room;
 
-import io.livekit.server.*;
-import jakarta.annotation.PostConstruct;
-import livekit.LivekitModels;
-import livekit.LivekitWebhook;
+import com.example.pinokkio.api.room.dto.request.RoomEnterRequest;
+import com.example.pinokkio.api.room.dto.request.RoomInfo;
+import io.livekit.server.AccessToken;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.socket.TextMessage;
-import org.springframework.web.socket.WebSocketSession;
-import retrofit2.Call;
-import retrofit2.Response;
 
-import java.io.IOException;
 import java.util.Collections;
-import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 @CrossOrigin(origins = "*") // 추후 수정
 @RestController
@@ -29,43 +18,10 @@ import java.util.concurrent.ConcurrentHashMap;
 @Slf4j
 @RequestMapping("/api/meeting")
 public class RoomController {
-    //TODO: 불필요한 메서드 제거 및 추가, 서비스 로직 분리
+// TODO: DTO, 리턴타입, URL
+
     private final RoomService roomService;
-
-    @Value("${livekit.api.key}")
-    private String LIVEKIT_API_KEY;
-
-    @Value("${livekit.api.secret}")
-    private String LIVEKIT_API_SECRET;
-
-    private RoomServiceClient roomServiceClient;
-
-    @PostConstruct
-    public void init() {
-        this.roomServiceClient = RoomServiceClient.create("http://localhost:7880/", LIVEKIT_API_KEY, LIVEKIT_API_SECRET);
-    }
-
-    private final ConcurrentHashMap<String, WebSocketSession> sessions = new ConcurrentHashMap<>();
-
-    // WebSocket 핸들러에서 호출될 메서드
-    public void handleWebSocketMessage(WebSocketSession session, TextMessage message) {
-        try {
-            JSONObject jsonMessage = new JSONObject(message.getPayload());
-            if ("participantInfo".equals(jsonMessage.getString("type"))) {
-                String participantName = jsonMessage.getString("participantName");
-                sessions.put(participantName, session);
-            }
-        } catch (JSONException e) {
-            log.info("[handleWebSocketMessage] ERROR:" + e.getMessage());
-        }
-    }
-
-    /**
-     * Token 생성
-     */
-    private AccessToken getToken() {
-        return new AccessToken(LIVEKIT_API_KEY, LIVEKIT_API_SECRET);
-    }
+    private final WebSocketService webSocketService;
 
     /**
      * 상담원 - 방 생성
@@ -75,7 +31,7 @@ public class RoomController {
     @PostMapping(value = "/{tellerId}")
     public ResponseEntity<Map<String, String>> createRoom(@PathVariable String tellerId) {
 
-        AccessToken roomToken = roomService.createRoom(getToken(), tellerId);
+        AccessToken roomToken = roomService.createRoom(tellerId);
         return ResponseEntity.ok(Map.of("token", roomToken.toJwt()));
     }
 
@@ -92,139 +48,85 @@ public class RoomController {
     }
 
     /**
-     * 상담원 - 고객의 상담 요청 승인
+     * 상담원 - 고객의 상담 요청 승인 및 입장을 위한 방 정보 제공
      */
-    @PreAuthorize("hasAnyRole('ROLE_TELLER')")
-    @PostMapping("/accept")
-    public ResponseEntity<Map<String, String>> acceptInvitation(@RequestBody Map<String, String> params) {
-        log.info("AcceptInvitation: {}", params);
-        String roomName = params.get("roomName");
-        String participantName = params.get("requestParticipantName");
+    @PreAuthorize("hasRole('ROLE_TELLER')")
+    @PostMapping("/{tellerId}/accept")
+    public ResponseEntity<Map<String, String>> acceptInvitation(@RequestBody RoomEnterRequest enterRequest, @PathVariable String tellerId) {
+        String roomId = enterRequest.getRoomId();
+        String kioskId = enterRequest.getUserId();
 
-        if (roomName == null || participantName == null) {
-            log.info("Missing roomName or participantName");
-            return ResponseEntity.badRequest().body(Collections.singletonMap("error", "Missing roomName or participantName"));
+        if (roomId == null || kioskId == null || tellerId == null) {
+            log.info("Missing roomId, kioskId, or tellerId");
+            return ResponseEntity.badRequest().body(Collections.singletonMap("error", "Missing required parameters"));
         }
 
-        WebSocketSession session = sessions.get(participantName);
-        if (session == null || !session.isOpen()) {
-            return ResponseEntity.status(404).body(Collections.singletonMap("error", "Session not found or closed"));
+        // 입장 가능 여부 검증
+        if (webSocketService.isTokenIssued(kioskId)) {
+            return ResponseEntity.status(400).body(Map.of("error", "Token already issued for this participant"));
         }
+        String acceptRoomId = roomService.acceptInvitation(roomId, tellerId, kioskId);
 
-        // 이미 토큰을 발급받은 참가자인지 확인
-        if (session.getAttributes().containsKey("tokenIssued")) {
-            return ResponseEntity.status(400).body(Collections.singletonMap("error", "Token already issued for this participant"));
+        // roomId 전송
+        try {
+            webSocketService.sendRoomId(kioskId, acceptRoomId);
+            return ResponseEntity.ok(Map.of("message", "RoomId sent successfully"));
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(500).body(Map.of("error", e.getMessage()));
         }
-
-        // 승인받은 방 제목으로 토큰 생성
-        AccessToken token = new AccessToken(LIVEKIT_API_KEY, LIVEKIT_API_SECRET);
-        token.setName(roomName);
-        token.setIdentity(participantName);
-        token.addGrants(new RoomJoin(true), new RoomName(roomName));
-
-        // 토큰 발급 표시
-        session.getAttributes().put("tokenIssued", true);
-        log.info("[AcceptInvitation] 토큰 발급 roomName: {} participantName: {}", roomName, participantName);
-        return ResponseEntity.ok(Map.of("token", token.toJwt()));
     }
 
     /**
      * 상담원 - 고객의 상담 요청 거절
      */
-    @PreAuthorize("hasAnyRole('ROLE_KIOSK')")
+    @PreAuthorize("hasRole('ROLE_KIOSK')")
     @PostMapping("/reject")
-    public ResponseEntity<String> rejectInvitation(@RequestBody Map<String, String> params) {
-        String roomName = params.get("roomName");
-        String participantName = params.get("participantName");
+    public ResponseEntity<String> rejectInvitation(@RequestBody RoomInfo roomInfo) {
+        String roomId = roomInfo.getRoomId();
+        String tellerId = roomInfo.getTellerId();
 
-        if (roomName == null || participantName == null) {
-            return ResponseEntity.badRequest().body("Missing roomName or participantName");
+        if (roomId == null || tellerId == null) {
+            return ResponseEntity.badRequest().body("Missing roomId or tellerId");
         }
 
-        // Perform actions needed to reject the invitation
-        return ResponseEntity.ok("Invitation rejected for room: " + roomName);
+        roomService.rejectInvitation(roomId, tellerId);
+        return ResponseEntity.ok("Invitation rejected for room: " + roomId);
     }
 
     /**
      * 고객 - 상담 요청 전송
      */
     @PreAuthorize("hasRole('ROLE_KIOSK')")
-    @PostMapping("/messageAllRooms")
-    public ResponseEntity<String> messageAllRooms(@RequestBody Map<String, String> params) {
-
-        String message = params.get("message");
-        String participantName = params.get("participantName");
-
-        if (message == null || participantName == null) {
-            return ResponseEntity.badRequest().body("Message and participantName are required");
+    @PostMapping("/{kioskId}")
+    public ResponseEntity<String> messageAllRooms(@PathVariable String kioskId) {
+        if (kioskId == null) {
+            return ResponseEntity.badRequest().body("Message and kioskId are required");
         }
 
-        log.info("Message: {}", message);
-        log.info("ParticipantName: {}", participantName);
-
+        // 모든 방에 상담 요청 전송
         try {
-            Call<List<LivekitModels.Room>> roomListCall = roomServiceClient.listRooms();
-            Response<List<LivekitModels.Room>> response = roomListCall.execute();
-
-            if (!response.isSuccessful()) {
-                return ResponseEntity.status(500).body("Failed to fetch rooms");
-            }
-
-            List<LivekitModels.Room> rooms = response.body();
-
-            // 모든 방에 알림 전송
-            for (LivekitModels.Room room : rooms) {
-                LivekitModels.DataPacket.Kind kind = LivekitModels.DataPacket.Kind.RELIABLE;
-                List<String> destinationIdentities = List.of(); // All participants
-
-                String jsonPayload = "{\"message\": \"" + message + "\", \"participantName\": \"" + participantName + "\"}";
-                Call<Void> call = roomServiceClient.sendData(room.getName(), jsonPayload.getBytes(), kind, destinationIdentities);
-                Response<Void> sendResponse = call.execute();
-                if (!sendResponse.isSuccessful()) {
-                    System.err.println("Failed to send message to room: " + room.getName() + " with response: " + sendResponse.message());
-                } else {
-                    log.info("Message sent to room: {}", room.getName());
-                }
-            }
+            roomService.messageAllRooms(kioskId);
             return ResponseEntity.ok("Message sent to all rooms");
-        } catch (Exception e) {
-            log.info("[messageAllRooms] ERROR:{}", e.getMessage());
-            return ResponseEntity.status(500).body("Failed to send message to all rooms");
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(500).body("Failed to send message to all rooms: " + e.getMessage());
         }
     }
 
     /**
-     * 상담원이 고객의 상담 요청 수락시 해당 정보를 바탕으로 고객에게 토큰 전송
+     * 고객 - 입장 요청
      */
-    @PreAuthorize("hasAnyRole('ROLE_KIOSK')")
-    @PostMapping("/sendToken")
-    public ResponseEntity<String> sendToken(@RequestBody Map<String, String> params) {
-        String participantName = params.get("participantName");
-        String token = params.get("token");
+    @PreAuthorize("hasRole('ROLE_KIOSK')")
+    @PostMapping("/enter")
+    public ResponseEntity<?> enterRoom(@RequestBody RoomEnterRequest enterRequest) {
+        String roomId = enterRequest.getRoomId();
+        String kioskId = enterRequest.getUserId();
 
-        if (participantName == null || token == null) {
-            return ResponseEntity.badRequest().body("ParticipantName and token are required");
+        if (roomId == null || kioskId == null) {
+            return ResponseEntity.badRequest().body("Missing roomId or kioskId");
         }
 
-        WebSocketSession session = sessions.get(participantName);
-        log.info("[sendToken] session: " + session);
-        if (session != null && session.isOpen()) {
-
-            try {
-                JSONObject jsonMessage = new JSONObject();
-                jsonMessage.put("type", "token");
-                jsonMessage.put("token", token);
-                session.sendMessage(new TextMessage(jsonMessage.toString()));
-
-                log.info("토큰 전송 성공!");
-                return ResponseEntity.ok("Token sent");
-            } catch (IOException | JSONException e) {
-                log.info("[sendToken] ERROR:{}", e.getMessage());
-                return ResponseEntity.status(500).body("Failed to send token");
-            }
-        } else {
-            return ResponseEntity.status(404).body("Session not found or closed");
-        }
+        AccessToken roomToken = roomService.enterRoom(roomId, kioskId);
+        return ResponseEntity.ok(Map.of("token", roomToken.toJwt()));
     }
 
     /**
@@ -239,16 +141,16 @@ public class RoomController {
         return ResponseEntity.ok().build();
     }
 
-
+    /**
+     * WebHook 이벤트 수신
+     */
     @PostMapping(value = "/livekit/webhook", consumes = "application/webhook+json")
     public ResponseEntity<String> receiveWebhook(@RequestHeader("Authorization") String authHeader, @RequestBody String body) {
-        WebhookReceiver webhookReceiver = new WebhookReceiver(LIVEKIT_API_KEY, LIVEKIT_API_SECRET);
         try {
-            LivekitWebhook.WebhookEvent event = webhookReceiver.receive(body, authHeader);
-            log.info("LiveKit Webhook: {}", event);
-        } catch (Exception e) {
-            System.err.println("Error validating webhook event: " + e.getMessage());
+            roomService.handleWebhook(authHeader, body);
+            return ResponseEntity.ok("Webhook processed successfully");
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(500).body("Failed to process webhook: " + e.getMessage());
         }
-        return ResponseEntity.ok("ok");
     }
 }
--- backend/pinokkio/src/main/java/com/example/pinokkio/api/room/RoomService.java
+++ backend/pinokkio/src/main/java/com/example/pinokkio/api/room/RoomService.java
@@ -4,112 +4,195 @@ import com.example.pinokkio.api.kiosk.Kiosk;
 import com.example.pinokkio.api.kiosk.KioskRepository;
 import com.example.pinokkio.api.teller.Teller;
 import com.example.pinokkio.api.teller.TellerRepository;
+import com.example.pinokkio.exception.RoomNotAvailableException;
 import com.example.pinokkio.exception.notFound.KioskNotFoundException;
 import com.example.pinokkio.exception.notFound.RoomNotFoundException;
 import com.example.pinokkio.exception.notFound.TellerNotFoundException;
-import io.livekit.server.AccessToken;
-import io.livekit.server.RoomJoin;
-import io.livekit.server.RoomName;
+import io.livekit.server.*;
+import jakarta.annotation.PostConstruct;
+import livekit.LivekitModels;
+import livekit.LivekitWebhook;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
 import java.util.function.Function;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
+@Slf4j
 @Transactional(readOnly = true)
 public class RoomService {
-    //TODO: 불필요한 메서드 제거 및 추가
+//TODO: 상담원, 고객 서비스 로직 분리
+//TODO: 존재 여부 검증 당위성
 
+    // 상담원의 최대 응대 고객 수
     private final int MAX_CAPACITY = 3;
 
     private final RoomRepository roomRepository;
     private final TellerRepository tellerRepository;
     private final KioskRepository kioskRepository;
 
-    @Transactional
-    public AccessToken createRoom(AccessToken roomToken, String tellerId) {
+    private RoomServiceClient roomServiceClient;
 
+    @Value("${livekit.api.key}")
+    private String LIVEKIT_API_KEY;
+
+    @Value("${livekit.api.secret}")
+    private String LIVEKIT_API_SECRET;
+
+    @PostConstruct
+    public void init() {
+        this.roomServiceClient = RoomServiceClient.create("http://localhost:7880/", LIVEKIT_API_KEY, LIVEKIT_API_SECRET);
+    }
+
+    /**
+     * 상담원 - 방 생성 후 입장을 위한 토큰 발급
+     */
+    @Transactional
+    public AccessToken createRoom(String tellerId) {
+        // 상담원 존재 여부 검증
         Teller teller = getEntityById(tellerRepository, tellerId, TellerNotFoundException::new);
-        // Room 생성
-        Room room = Room.builder()
-                .teller(teller)
-                .build();
+
+        // 방 생성 후 DB 저장
+        Room room = Room.builder().
+                teller(teller).
+                build();
         roomRepository.save(room);
 
         // 토큰 발급
         String roomId = room.getRoomId().toString();
-        roomToken.setName(roomId);
-        roomToken.setIdentity(tellerId);
-        roomToken.addGrants(new RoomJoin(true), new RoomName(roomId));
-
+        AccessToken roomToken = createToken(roomId, tellerId);
         log.info("[createRoom] roomId: {}, tellerId: {}", roomId, tellerId);
+
         return roomToken;
     }
 
+    /**
+     * 상담원 - 상담원 퇴장 시 방 삭제
+     */
     @Transactional
     public void deleteRoom(String tellerId) {
-
         Teller teller = getEntityById(tellerRepository, tellerId, TellerNotFoundException::new);
         roomRepository.deleteByTeller(teller);
-
-        log.info("[deleteRoom] complete");
+        log.info("[deleteRoom] complete for tellerId: {}", tellerId);
     }
 
-    @Transactional
-    public AccessToken enterRoom(AccessToken roomToken, String kioskId) {
-
+    /**
+     * 상담원 - 상담 요청 수락시 고객의 방 접근을 위한 roomId 정보 제공
+     */
+    public String acceptInvitation(String roomId, String tellerId, String kioskId) {
+        Room room = getEntityById(roomRepository, roomId, RoomNotFoundException::new);
+        Teller teller = getEntityById(tellerRepository, tellerId, TellerNotFoundException::new);
         Kiosk kiosk = getEntityById(kioskRepository, kioskId, KioskNotFoundException::new);
 
-        // 입장 가능한 방 찾기
-        String roomId = getAvailableRoom();
-        log.info("[enterRoom] available roomId: {}", roomId);
+        log.info("[acceptInvitation] roomId: {}, participantName: {}", roomId, tellerId);
+        return roomId;
+    }
 
+    /**
+     * 상담원 - 상담 요청 거절
+     */
+    public void rejectInvitation(String roomId, String tellerId) {
+        log.info("[rejectInvitation] roomId: {}, tellerId: {}", roomId, tellerId);
+    }
+
+    /**
+     * 고객 -모든 방에 상담 요청 전송
+     */
+    public void messageAllRooms(String kioskId) {
+        try {
+            List<LivekitModels.Room> rooms = roomServiceClient.listRooms().execute().body();
+            for (LivekitModels.Room room : rooms) {
+                // 입장 요청 전송
+                sendMessageToRoom(room, kioskId);
+            }
+        } catch (IOException e) {
+            log.error("[messageAllRooms] Error: {}", e.getMessage());
+            throw new RuntimeException("Failed to send message to all rooms", e);
+        }
+    }
+
+    /**
+     * 고객 - 입장
+     */
+    @Transactional
+    public AccessToken enterRoom(String roomId, String kioskId) {
+        // 고객 존재 여부 검증
+        Kiosk kiosk = getEntityById(kioskRepository, kioskId, KioskNotFoundException::new);
+
+        // 방 유효성 검증
         Room room = getEntityById(roomRepository, roomId, RoomNotFoundException::new);
-        room.setNumberOfCustomers(room.getNumberOfCustomers() + 1);
+        int currentCustomerCount = room.getNumberOfCustomers();
+        if (currentCustomerCount >= MAX_CAPACITY) {
+            throw new RoomNotAvailableException(UUID.fromString(roomId));
+        }
+        room.updateNumberOfCustomers(currentCustomerCount+1);
         roomRepository.save(room);
 
         // 토큰 발급
-        roomToken.setName(roomId);
-        roomToken.setIdentity(kioskId);
-        roomToken.addGrants(new RoomJoin(true), new RoomName(roomId));
+        AccessToken roomToken = createToken(roomId, kioskId);
+        log.info("[createRoom] roomId: {}, kioskId: {}", roomId, kioskId);
 
-        log.info("[enterRoom] roomId: {}, kioskId: {}", roomId, kioskId);
         return roomToken;
     }
 
-    private String getAvailableRoom() {
-
-        List<Room> availableRooms = roomRepository.findAllByOrderByNumberOfCustomersAsc();
-
-        Room firstRoom = availableRooms.getFirst();
-        if (firstRoom.getNumberOfCustomers() < MAX_CAPACITY) {
-            return firstRoom.getRoomId().toString();
-        }
-
-        log.info("[getAvailableRoom] 모든 방이 {}명 상담 중", MAX_CAPACITY);
-        return "";
-    }
-
+    /**
+     * 고객 - 방 퇴장
+     */
     @Transactional
     public void leaveRoom(String roomId) {
         Room room = getEntityById(roomRepository, roomId, RoomNotFoundException::new);
-        int curCusNum = room.getNumberOfCustomers();
-        log.info("[leaveRoom] Number of Customers: {}", curCusNum);
 
-        if (curCusNum > 0) {
-            room.setNumberOfCustomers(curCusNum - 1);
-            log.info("[leaveRoom] Number of Customers: {}", curCusNum - 1);
+        int currentCustomerCount = room.getNumberOfCustomers();
+        if (currentCustomerCount > 0) {
+            room.updateNumberOfCustomers(currentCustomerCount - 1);
             roomRepository.save(room);
+        }
+        log.info("[leaveRoom] roomId: {}, new number of customers: {}", roomId, room.getNumberOfCustomers());
+    }
+
+    public void handleWebhook(String authHeader, String body) {
+        WebhookReceiver webhookReceiver = new WebhookReceiver(LIVEKIT_API_KEY, LIVEKIT_API_SECRET);
+        try {
+            LivekitWebhook.WebhookEvent event = webhookReceiver.receive(body, authHeader);
+            log.info("LiveKit Webhook: {}", event);
+        } catch (Exception e) {
+            log.error("Error validating webhook event: {}", e.getMessage());
+            throw new RuntimeException("Failed to handle webhook", e);
         }
     }
 
+    /**
+     * roomID, userId롤 openVidu 접근을 위한 토큰 생성
+     */
+    private AccessToken createToken(String roomId, String userId) {
+        AccessToken token = new AccessToken(LIVEKIT_API_KEY, LIVEKIT_API_SECRET);
+        token.setName(roomId);
+        token.setIdentity(userId);
+        token.addGrants(new RoomJoin(true), new RoomName(roomId));
+        return token;
+    }
+
+    /**
+     * Room 메시지 송신 메서드
+     *
+     * @param room
+     * @param participantName 송신자
+     */
+    private void sendMessageToRoom(LivekitModels.Room room, String participantName) throws IOException {
+        LivekitModels.DataPacket.Kind kind = LivekitModels.DataPacket.Kind.RELIABLE;
+        List<String> destinationIdentities = List.of();
+        String jsonPayload = String.format("{\"participantName\": \"상담 요청: %s\"}", participantName);
+        roomServiceClient.sendData(room.getName(), jsonPayload.getBytes(), kind, destinationIdentities).execute();
+        log.info("Message sent to room: {}", room.getName());
+    }
 
     /**
      * 제네릭 메서드를 사용하여 다양한 엔티티를 조회하는 로직
