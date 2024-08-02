92b8a45b96f83dde0e78a918be38b79b1cad653a
전용수 <1010qazs2@gmail.com>
2024-08-02 15:12:40 +0900

    [feat] 키오스크 본인정보 조회 추가

--- backend/pinokkio/src/main/java/com/example/pinokkio/api/kiosk/KioskController.java
+++ backend/pinokkio/src/main/java/com/example/pinokkio/api/kiosk/KioskController.java
@@ -3,16 +3,21 @@ package com.example.pinokkio.api.kiosk;
 import com.example.pinokkio.api.kiosk.dto.response.KioskResponse;
 import com.example.pinokkio.api.pos.dto.response.PosResponse;
 import com.example.pinokkio.config.jwt.JwtProvider;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
+@Tag(name = "Kiosk Controller", description = "키오스크 정보 관련 API")
 public class KioskController {
 
     private final KioskService kioskService;
@@ -22,6 +27,7 @@ public class KioskController {
      * 요청한 키오스크의 자기 정보 조회
      * [연결된 포스 ID, 키오스크 본인 ID, 키오스크 이메일]
      */
+    @Operation(summary = "키오스크 본인정보 조회", description = "키오스크 본인정보 조회")
     @PreAuthorize("hasRole('ROLE_KIOSK')")
     @GetMapping("/kiosk/my-info")
     public ResponseEntity<?> getKiosk() {
--- backend/pinokkio/src/main/java/com/example/pinokkio/api/pos/PosController.java
+++ backend/pinokkio/src/main/java/com/example/pinokkio/api/pos/PosController.java
@@ -2,16 +2,21 @@ package com.example.pinokkio.api.pos;
 
 import com.example.pinokkio.api.pos.dto.response.PosResponse;
 import com.example.pinokkio.config.jwt.JwtProvider;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
+@Tag(name = "Pos Controller", description = "포스 정보 관련 API")
 public class PosController {
 
     private final PosService posService;
@@ -21,6 +26,7 @@ public class PosController {
      * 요청한 포스의 자기 정보 조회
      * [가맹점 이름, 포스 ID, 포스 이메일]
      */
+    @Operation(summary = "포스 본인정보 조회", description = "포스 본인정보 조회")
     @PreAuthorize("hasRole('ROLE_POS')")
     @GetMapping("/pos/my-info")
     public ResponseEntity<?> getPos() {
