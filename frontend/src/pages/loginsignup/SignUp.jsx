a971e16566c509525061f1089e5f722300ace66e
sangmu <dltkdandl@naver.com>
2024-08-06 16:24:30 +0900

    feat: register form api connect

--- frontend/src/pages/loginsignup/SignUp.jsx
+++ frontend/src/pages/loginsignup/SignUp.jsx
@@ -3,7 +3,12 @@ import Logo from '../../components/common/Logo';
 import styled from 'styled-components';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import {
+  posDuplicateEmail,
+  tellerDuplicateEmail,
+  postRegisterAdvisor,
+  postRegisterPos,
+} from '../../apis/Auth';
 
 const SignUpWrapper = styled.div`
   display: flex;
@@ -101,54 +106,65 @@ function SignUp() {
   const [isUsableId, setIsUsableId] = useState(false);
   const [password, setPassword] = useState('');
   const [passwordChecking, setPasswordChecking] = useState('');
-  const [email, setEmail] = useState('');
-  const [selected, setSelected] = useState('');
+  const [position, setPosition] = useState('');
+  const [code, setCode] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
   const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const user = useSelector((state) => state.user.user);
 
-  const checkIdDuplicated = (e) => {
-    setId(e.target.value);
+  const checkIdDuplicated = async (e) => {
+    const email = e.target.value;
+    setId(email);
     setIsUsableId(false);
-    // 아이디 바뀔 때마다 가져옴
-    axios
-      .post('url', {
-        id: id,
-      })
-      .then((response) => {
-        // response
-        if (response.isUsable) {
+    setErrorMessage('');
+
+    if (position) {
+      try {
+        const response =
+          position === 'pos' ? await posDuplicateEmail(email) : await tellerDuplicateEmail(email);
+        if (response.duplicate) {
+          setErrorMessage('사용할 수 없는 이메일입니다.');
+        } else {
           setIsUsableId(true);
         }
-      })
-      .catch((error) => {
-        // 오류발생시 실행
-      })
-      .then(() => {
-        // 항상 실행
-      });
+      } catch (error) {
+        setErrorMessage('이메일 중복 검사 중 오류가 발생했습니다.');
+      }
+    } else {
+      setErrorMessage('직책을 먼저 선택해주세요.');
+    }
   };
 
-  const handleSignUp = () => {
-    axios
-      .post('url', {
-        id: id,
-        password: password,
-      })
-      .then((response) => {
-        // response
-        if (response.isUsable) {
-          setIsUsableId(true);
-        }
-      })
-      .catch((error) => {
-        // 오류발생시 실행
-      })
-      .then(() => {
-        // 항상 실행
-      });
-    navigate('/');
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    if (!isUsableId) {
+      alert('이메일 중복 검사를 완료해주세요.');
+      return;
+    }
+    if (password !== passwordChecking) {
+      alert('비밀번호가 일치하지 않습니다.');
+      return;
+    }
+    if (password.length < 8) {
+      alert('비밀번호를 8자리 이상으로 설정해주세요.');
+      return;
+    }
+    try {
+      if (position === 'advisor') {
+        console.log('상담원 로그인 중');
+        await postRegisterAdvisor(code, id, password, passwordChecking);
+      } else if (position === 'pos') {
+        console.log('포스 로그인 중');
+        console.log(`code : ${code}`);
+        console.log(`id : ${id}`);
+        console.log(`password : ${password}`);
+        console.log(`passwordChecking : ${passwordChecking}`);
+        await postRegisterPos(code, id, password, passwordChecking);
+      }
+      navigate('/');
+    } catch (error) {
+      alert('회원가입 중 오류가 발생했습니다. 다시 시도해주세요.');
+    }
   };
 
   return (
@@ -158,17 +174,34 @@ function SignUp() {
         <SignUpTitle>회원가입</SignUpTitle>
 
         <LabelContainer>
+          <SelectInput
+            name="position"
+            id="position"
+            onChange={(e) => {
+              setPosition(e.target.value);
+              setErrorMessage('');
+              setIsUsableId(false);
+            }}
+          >
+            <option value="" selected disabled hidden>
+              선택해주세요
+            </option>
+            <option value="pos">포스기</option>
+            <option value="advisor">상담원</option>
+          </SelectInput>
+        </LabelContainer>
+
+        <LabelContainer>
           <Input
-            id="id"
             type="text"
-            className="has-label"
-            placeholder="아이디"
+            className="Email"
+            placeholder="이메일"
             onChange={(e) => checkIdDuplicated(e)}
+            disabled={!position}
           />
-          {isUsableId ? (
-            <AddedMessage isUsableId={isUsableId}>사용할 수 있는 아이디입니다.</AddedMessage>
-          ) : (
-            <AddedMessage isUsableId={isUsableId}>사용할 수 없는 아이디입니다.</AddedMessage>
+          {errorMessage && <AddedMessage>{errorMessage}</AddedMessage>}
+          {isUsableId && !errorMessage && (
+            <AddedMessage isUsableId={isUsableId}>사용할 수 있는 이메일입니다.</AddedMessage>
           )}
         </LabelContainer>
 
@@ -178,19 +211,22 @@ function SignUp() {
           className="Password"
           placeholder="패스워드"
           onChange={(e) => setPassword(e.target.value)}
+          disabled={!position}
         />
         <LabelContainer>
           <Input
-            name
             id="password-checking"
             type="password"
             className="has-label"
             placeholder="패스워드 확인"
             onChange={(e) => setPasswordChecking(e.target.value)}
+            disabled={!position}
           />
           {password.length >= 8 ? (
             password === passwordChecking ? (
-              <AddedMessage isCorrect={true}>비밀번호가 일치합니다.</AddedMessage>
+              <AddedMessage isCorrect={true} style={{ color: 'green' }}>
+                비밀번호가 일치합니다.
+              </AddedMessage>
             ) : (
               <AddedMessage isCorrect={false}>비밀번호가 일치하지 않습니다.</AddedMessage>
             )
@@ -198,30 +234,20 @@ function SignUp() {
             <AddedMessage isCorrect={false}>비밀번호를 8자리 이상으로 설정해주세요.</AddedMessage>
           )}
         </LabelContainer>
-        <Input type="text" className="Email" placeholder="이메일" />
-        <SelectInput name="position" id="position">
-          <option value="" selected disabled hidden>
-            선택해주세요
-          </option>
-          <option value="pos">포스기</option>
-          <option value="advisor">상담원</option>
-        </SelectInput>
-        <StyledButton
-          type="submit"
-          //   disabled={
-          //     () => {
-          //     return (
-          //       isUsableId &&
-          //       password.length >= 8 &&
-          //       password === passwordChecking &&
-          //       email &&
-          //       selected
-          //     );
-          //   }
-          // }
-        >
-          회원가입
-        </StyledButton>
+
+        <LabelContainer>
+          <Input
+            type="text"
+            className="code"
+            placeholder={
+              position === 'pos' ? '매장 코드를 입력해주세요.' : '포스 코드를 입력해주세요.'
+            }
+            onChange={(e) => setCode(e.target.value)}
+            disabled={!position}
+          />
+        </LabelContainer>
+
+        <StyledButton type="submit">회원가입</StyledButton>
       </SignUpForm>
     </SignUpWrapper>
   );
