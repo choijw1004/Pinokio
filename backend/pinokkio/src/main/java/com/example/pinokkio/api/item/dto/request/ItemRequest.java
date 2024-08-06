b06268413dc32e435ef130da92fbacf3c5c1707b
yeonseo <yeonseo.jung0@gmail.com>
2024-08-06 13:47:07 +0900

    [fix] file, posId 필드 제거

--- backend/pinokkio/src/main/java/com/example/pinokkio/api/item/dto/request/ItemRequest.java
+++ backend/pinokkio/src/main/java/com/example/pinokkio/api/item/dto/request/ItemRequest.java
@@ -14,10 +14,6 @@ import org.springframework.web.multipart.MultipartFile;
 @Schema(description = "아이템 등록 요청 DTO")
 public class ItemRequest {
 
-    @NotEmpty(message = "posId는 필수 값입니다.")
-    @Schema(description = "포스 ID", example = "123e4567-e89b-12d3-a456-426614174000")
-    private String posId;
-
     @NotEmpty(message = "categoryId는 필수 값입니다.")
     @Schema(description = "카테고리 ID", example = "123e4567-e89b-12d3-a456-426614174000")
     private String categoryId;
@@ -37,7 +33,4 @@ public class ItemRequest {
     @NotEmpty(message = "detail은 필수 값입니다.")
     @Schema(description = "아이템 세부 사항", example = "참깨빵 위에 순 쇠고기 패티 두 장 특별한 소스 양상추 치즈 피클 양파 까~지")
     private String detail;
-
-    @Schema(description = "아이템 이미지 파일")
-    private MultipartFile file;
 }
