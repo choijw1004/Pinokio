c21e8fc6bf5b32459610b3986096302bb66ec561
97choijw <97choijw@gmail.com>
2024-08-06 16:06:27 +0900

    [hotfix] ing

--- backend/pinokkio/src/main/java/com/example/pinokkio/config/WebConfig.java
+++ backend/pinokkio/src/main/java/com/example/pinokkio/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("https://i11a601.p.ssafy.io")
+                .allowedOrigins("https://i11a601.p.ssafy.io")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
