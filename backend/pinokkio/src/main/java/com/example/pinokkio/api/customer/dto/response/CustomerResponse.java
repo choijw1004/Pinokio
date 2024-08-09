bf57c5c459e03dad5edfb9d6ad1f4babbb51367a
yeonseo <yeonseo.jung0@gmail.com>
2024-08-09 13:09:13 +0900

    [refactor] 응답 데이터 필드 수정 posId -> customerId

--- backend/pinokkio/src/main/java/com/example/pinokkio/api/customer/dto/response/CustomerResponse.java
+++ backend/pinokkio/src/main/java/com/example/pinokkio/api/customer/dto/response/CustomerResponse.java
@@ -12,8 +12,8 @@ import java.util.UUID;
 @Schema(description = "고객 응답 데이터")
 public class CustomerResponse {
 
-    @Schema(description = "포스 ID", example = "123e4567-e89b-12d3-a456-426614174000")
-    private final UUID posId;
+    @Schema(description = "고객 ID", example = "123e4567-e89b-12d3-a456-426614174000")
+    private final UUID customerId;
 
     @Schema(description = "성별", example = "FEMALE")
     private final Gender gender;
@@ -24,15 +24,8 @@ public class CustomerResponse {
     @Schema(description = "나이", example = "20")
     private final int age;
 
-    public CustomerResponse(Item item, Gender gender, String phoneNumber, int age) {
-        this.posId = item.getPos().getId();
-        this.gender = gender;
-        this.phoneNumber = phoneNumber;
-        this.age = age;
-    }
-
     public CustomerResponse(Customer customer) {
-        this.posId = customer.getPos().getId();
+        this.customerId = customer.getId();
         this.gender = customer.getGender();
         this.phoneNumber = customer.getPhoneNumber();
         this.age = customer.getAge();
