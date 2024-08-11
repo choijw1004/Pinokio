b971e28aca951332d113ee5b8ac1ee6babddbbd6
rlawnsdn <junoo1997@naver.com>
2024-08-11 20:34:19 +0900

    merge local frontend to origin/develop

--- frontend/src/components/pos/ToggleIcon.jsx
+++ frontend/src/components/pos/ToggleIcon.jsx
@@ -8,98 +8,20 @@ const ToggleIcon = ({ isOn, setIsOn }) => {
   };
 
   return (
-    <div onClick={toggleIcon} style={{ cursor: 'pointer', width: '64px', height: '64px' }}>
+    <div
+      onClick={toggleIcon}
+      style={{
+        cursor: 'pointer',
+        width: '64px',
+        height: '64px',
+        display: 'flex',
+        justifyContent: 'center',
+      }}
+    >
       {isOn ? (
-        <svg
-          width="64"
-          height="64"
-          viewBox="0 0 64 64"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <rect
-            x="11.5"
-            y="11.5"
-            width="17"
-            height="17"
-            rx="1.5"
-            stroke="#7392FF"
-            stroke-width="3"
-          />
-          <rect
-            x="31.9787"
-            y="20"
-            width="17"
-            height="17"
-            rx="1.5"
-            transform="rotate(-45 31.9787 20)"
-            stroke="#EC7348"
-            stroke-width="3"
-          />
-          <rect
-            x="11.5"
-            y="35.5"
-            width="17"
-            height="17"
-            rx="1.5"
-            stroke="#C383D9"
-            stroke-width="3"
-          />
-          <rect
-            x="35.5"
-            y="35.5"
-            width="17"
-            height="17"
-            rx="1.5"
-            stroke="#FFC33F"
-            stroke-width="3"
-          />
-        </svg>
+        <img src="/NavBarOn.svg" width="45rem" />
       ) : (
-        <svg
-          width="64"
-          height="64"
-          viewBox="0 0 64 64"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <rect
-            x="11.5"
-            y="11.5"
-            width="17"
-            height="17"
-            rx="1.5"
-            stroke="#7392FF"
-            stroke-width="3"
-          />
-          <rect
-            x="35.5"
-            y="11.5"
-            width="17"
-            height="17"
-            rx="1.5"
-            stroke="#EC7348"
-            stroke-width="3"
-          />
-          <rect
-            x="11.5"
-            y="35.5"
-            width="17"
-            height="17"
-            rx="1.5"
-            stroke="#C383D9"
-            stroke-width="3"
-          />
-          <rect
-            x="35.5"
-            y="35.5"
-            width="17"
-            height="17"
-            rx="1.5"
-            stroke="#FFC33F"
-            stroke-width="3"
-          />
-        </svg>
+        <img src="/NavBarOff.svg" width="45rem" />
       )}
     </div>
   );
--- frontend/src/pages/loginsignup/Login.jsx
+++ frontend/src/pages/loginsignup/Login.jsx
@@ -81,17 +81,19 @@ const StyledButton = styled.button`
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
 
   &:hover {
-    transform: translateY(-2px);
+    transform: translateY(-1px);
+  }
+  &:hover {
+    background-color: #c383d9;
   }
   &:active {
-    background-color: #c383d9;
+    background-color: #7392ff;
   }
 `;
 
 const ButtonWrapper = styled.div`
   display: flex;
   flex-direction: row-reverse;
-
   margin-top: 20px;
 
   div {
@@ -102,6 +104,9 @@ const ButtonWrapper = styled.div`
 
     &:hover {
       color: #c383d9;
+    }
+    &:active {
+      color: #7392ff;
     }
   }
 `;
