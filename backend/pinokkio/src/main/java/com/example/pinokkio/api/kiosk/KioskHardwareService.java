ea8b7276d4119e6ff3cd5d9e996fd98f6d9dc81a
97choijw <97choijw@gmail.com>
2024-08-11 22:27:22 +0900

    ing

--- backend/pinokkio/src/main/java/com/example/pinokkio/api/kiosk/KioskHardwareService.java
+++ backend/pinokkio/src/main/java/com/example/pinokkio/api/kiosk/KioskHardwareService.java
@@ -28,7 +28,7 @@ public class KioskHardwareService extends KioskServiceGrpc.KioskServiceImplBase 
 
     private static final int MAX_BRIGHTNESS = 100;
     private static final String KIOSK_CONTROLLER_ADDRESS = "172.30.1.54";
-    private static final int KIOSK_CONTROLLER_PORT = 50052;
+    private static final int KIOSK_CONTROLLER_PORT = 3334;
     private static final int GRPC_SERVER_PORT = 3333;
 
     private Server grpcServer;
