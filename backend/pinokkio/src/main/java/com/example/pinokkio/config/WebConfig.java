ce3a4bf8ba8971e92d257e788f5a1819da7c6567
정연서 <yeonseo.jung0@gmail.com>
2024-08-13 23:11:59 +0900

    Update WebConfig.java

--- backend/pinokkio/src/main/java/com/example/pinokkio/config/WebConfig.java
+++ backend/pinokkio/src/main/java/com/example/pinokkio/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 // .allowedOriginPatterns("http://localhost:3000") 로컬용
-                .allowedOriginPatterns("http://i11a601.p.ssafy.io")
+                .allowedOriginPatterns("https://i11a601.p.ssafy.io")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
