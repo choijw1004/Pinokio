785303f4a1115ba8fb5241ada4c64a0f33e30383
97choijw <97choijw@gmail.com>
2024-08-14 14:18:05 +0900

    [hotfix] RealFinalClone

--- backend/pinokkio/src/main/java/com/example/pinokkio/config/WebConfig.java
+++ backend/pinokkio/src/main/java/com/example/pinokkio/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:3000")
+                .allowedOriginPatterns("https://i11a601.p.ssafy.io:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
