8f35d49b8bc21017dea1a6389be5625a6068bd88
97choijw <97choijw@gmail.com>
2024-08-15 15:44:21 +0900

    CORS 설정

--- backend/pinokkio/src/main/java/com/example/pinokkio/config/WebConfig.java
+++ backend/pinokkio/src/main/java/com/example/pinokkio/config/WebConfig.java
@@ -11,6 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOriginPatterns("https://i11a601.p.ssafy.io:3000")
+                .allowedOriginPatterns("https://pinokio-openvidu.duckdns.org")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
